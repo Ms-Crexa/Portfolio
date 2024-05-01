14ae95078acc2a5b99d33d1ce740b06869531947
MS-Crexa <vthelym@gmail.com>
2024-05-01 15:19:18 +0800

    Fix bugs

--- app/Pages/About/Components/Character/Character.tsx
+++ app/Pages/About/Components/Character/Character.tsx
@@ -1,13 +1,13 @@
-"use client";
+'use client';
 import React, { useRef } from "react";
-import { motion, useScroll, useTransform } from "framer-motion";
+import { motion, useScroll, useTransform, MotionValue } from "framer-motion";
 
 interface ParagraphProps {
-  value: string;
+ value: string;
 }
 
 export default function Paragraph({ value }: ParagraphProps) {
- const container = useRef(null);
+ const container = useRef<HTMLParagraphElement>(null);
  const { scrollYProgress } = useScroll({
   target: container,
   offset: ["start 0.9", "start 0.25"],
@@ -30,8 +30,13 @@ export default function Paragraph({ value }: ParagraphProps) {
  );
 }
 
+interface WordProps {
+ children: string;
+ range: [number, number];
+ progress: MotionValue<number>;
+}
 
-const Word = ({ children, range, progress }) => {
+const Word = ({ children, range, progress }: WordProps) => {
  const amount = range[1] - range[0];
  const step = amount / children.length;
 
@@ -39,7 +44,6 @@ const Word = ({ children, range, progress }) => {
   <span>
    {children.split("").map((char, i) => {
     const start = range[0] + i * step;
-
     const end = range[0] + (i + 1) * step;
 
     return (
@@ -52,7 +56,13 @@ const Word = ({ children, range, progress }) => {
  );
 };
 
-const Character = ({ children, range, progress }) => {
+interface CharacterProps {
+ children: string;
+ range: [number, number];
+ progress: MotionValue<number>;
+}
+
+const Character = ({ children, range, progress }: CharacterProps) => {
  const opacity = useTransform(progress, range, [0, 1]);
  return <motion.span style={{ opacity }}>{children}</motion.span>;
 };
